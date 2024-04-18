486a88fdf3cdc102da8f7376a04477718d821c95
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-04-18 21:18:12 +0200

    Added edit functionality

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,20 @@ app.delete("/users/:id", (req, res) => {
   res.send(users);
 });
 
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.find((u) => u.id === parseInt(id));
+  const { name, email, country, contact } = req.body;
+  if (!user) {
+    res.status(400).send("User not found!");
+  }
+  user.name = name;
+  (user.email = email),
+    (user.country = country),
+    (user.contact = contact),
+    res.send(users);
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
