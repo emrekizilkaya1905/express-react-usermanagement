197a4c64286e79a9081475402d0807f659072a12
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-04-18 14:52:00 +0200

    Create user and testeed on localhost

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,25 @@ const app = express();
 
 const port = 5000;
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send(users);
 });
+
+let users = [
+  {
+    id: 1,
+    name: "Emre",
+    email: "kizilkayaemre88@gmail.com",
+    country: "Turkey",
+    contact: "111222333",
+  },
+  {
+    id: 2,
+    name: "Ece",
+    email: "kizilkayaece01@gmail.com",
+    country: "Turkey",
+    contact: "444555666",
+  },
+];
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
